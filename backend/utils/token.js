749386430f1f357d5a4dca7edd1448a81bb4f8ed
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-12-03 20:51:08 +0800

    configured

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -2,26 +2,23 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv'
 
 dotenv.config();
-// Your secret key (store it securely and use environment variables)
 const secretKey = process.env.JWT_SECRET;
 
 // Function to generate a JWT token for a user
 export function generateUserToken(user) {
-  // User role should be 'user'
   const payload = {
     _id: user._id,
     role: 'user',
   };
   
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' }); // Token expires in 1 hour
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 }
 
 // Function to generate a JWT token for an admin
 export function generateAdminToken(admin) {
-  // Admin role should be 'admin'
   const payload = {
     _id: admin._id,
     role: 'admin',
   };
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' }); // Token expires in 1 hour
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 }
