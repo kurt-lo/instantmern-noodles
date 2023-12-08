73155d28c3249a1247294a6ceeb3f75727a1ce13
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-12-08 21:23:39 +0800

    Home added link

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -5,23 +5,36 @@ import { AuthContext } from '../context/AuthContext'
 const Header = () => {
 
   const { isAuthenticated, user, logout } = useContext(AuthContext);
+  const [dropdown, setDropdown] = useState(false);
 
   return (
-    <nav className="flex justify-between px-[2rem] py-[1rem] bg-slate-400">
+    <nav className="flex justify-between px-[2rem] py-[1rem] mb-[1rem] text-stone-950 font-[700]">
       <div>
         <Link to='/'>MERN+AUTH</Link>
       </div>
       {isAuthenticated ? (
-        <ul className="flex">
+        <ul className="flex gap-[2rem]">
           <li>
-            <h1>{user ? user.name : 'Loading...'}</h1>
+            <Link to='/home'>Home</Link>
+          </li>
+          <li>
+            <div>
+              <button onClick={() => setDropdown(!dropdown)} className='relative'>
+                {user ? `Welcome, ${user.name}!` : 'Loading...'}
+              </button>
+              {dropdown && (
+                <div className="profile absolute right-[120px] bg-white px-[1rem] py-[.1rem] rounded-[5px] text-center">
+                  <Link to='/profile'>User Profile</Link>
+                </div>
+              )}
+            </div>
           </li>
           <li>
             <Link to='/' onClick={logout}>Logout</Link>
           </li>
         </ul>
       ) : (
-        <ul className="flex">
+        <ul className="flex gap-[2rem]">
           <li>
             <Link to='/login'>Login</Link>
           </li>
