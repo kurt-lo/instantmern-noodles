cc4ec9c888113130e46c092fe75c3a7fb3f10144
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-12-23 12:42:32 +0800

    coming

---
+++ frontend/src/component/BestSeller.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BestSeller = () => {
+  return (
+    <div>Dito ko lalagay mga products na best seller</div>
+  )
+}
+
+export default BestSeller
---
+++ frontend/src/component/Rating.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rating = () => {
+  return (
+    <div>Dito ko lalagay mga rating ng mga customer</div>
+  )
+}
+
+export default Rating
