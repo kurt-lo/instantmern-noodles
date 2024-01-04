57b52a733d26acc4a79134aea0939388852dd334
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-01-04 19:10:30 +0800

    cors

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,7 @@ connectDB()
 const app = express();
 
 app.use(express.json())
-app.use(cors({
-    origin: ["https://mern-stack-frontend-omega.vercel.app"],
-    methods: ['POST', 'GET'],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
