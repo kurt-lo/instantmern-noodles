d0951d6bf948662179c93b55b71fde7cd9ce2769
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-01-04 22:45:54 +0800

    deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9999',
+        target: 'https://mern-stack-practice-liart.vercel.app',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     }
   }
