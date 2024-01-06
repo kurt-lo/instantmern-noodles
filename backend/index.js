a161c507a2bb4c981e1218c5e328dc8729f0e44d
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-01-06 18:59:47 +0800

    cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,11 @@ connectDB()
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://mern-stack-frontend-alpha.vercel.app',
+    credentials: true, // Include credentials in CORS request (if needed)
+  }));
+  
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
